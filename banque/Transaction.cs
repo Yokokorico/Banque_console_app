cdf6ac27266e93a9a22c3422cb5ce8497d60e419
Théo Duflos <theo.duflos.yokoko@gmail.com>
2023-12-04 14:26:27 +0100

    feat: :construction: Creation of the Transaction class

---
+++ banque/Transaction.cs
@@ -0,0 +1,8 @@
+namespace banque {
+    public class Transaction{
+        public string intitule{get; set;}
+        public int montant{get; set;}
+        public DateTime date {get; set;}
+
+    }
+}
