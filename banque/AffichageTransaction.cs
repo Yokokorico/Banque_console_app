1ff19b812799b8317fca8485fbe825e373224be3
Théo Duflos <theo.duflos.yokoko@gmail.com>
2023-12-06 10:57:48 +0100

    feat :sparkles: New features transaction history

---
+++ banque/AffichageTransaction.cs
@@ -0,0 +1,16 @@
+using banque;
+
+namespace Affichage;
+
+public class AffichageTransaction{
+
+    public static void AffichageLesTransactionDuCompte(Client client,int numeroCompte){
+        var DateMoinsUnMois = DateTime.Now;
+        DateMoinsUnMois = DateMoinsUnMois.AddMonths(-1);
+        var listeTransactionDuMois = client.comptes.Single(s => s.numeroCompte == numeroCompte).listeTransaction.Where(items => items.date > DateMoinsUnMois);
+        foreach (var transaction in listeTransactionDuMois)
+        {
+            Console.WriteLine($"Date: {transaction.date}, Intitulé: {transaction.intitule}, Montant: {transaction.montant}");
+        }
+    }
+}
