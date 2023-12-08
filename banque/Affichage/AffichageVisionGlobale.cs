9c06eb27339f388851603da729bb04bece75879a
Huluette <leana.beuvant@gmail.com>
2023-12-08 14:24:43 +0100

    refactor :recycle: supp code useless in VisionGlobale method

--- banque/Affichage/AffichageVisionGlobale.cs
+++ banque/Affichage/AffichageVisionGlobale.cs
@@ -15,26 +15,6 @@ public class AffichageVisionGlobale{
                 Console.WriteLine($"{index++}. {compte.numeroCompte}");
                 Console.WriteLine($"Type de compte : {compte.GetType().Name}");
                 Console.WriteLine($"Solde : {compte.solde}");
-
-                Console.WriteLine("Voulez-vous connaître la politique de vos comptes ? \n1. Oui \n2. Non");
-                if (!Int32.TryParse(Console.ReadLine(), out int reponse))
-                {
-                    Console.WriteLine("Entrée invalide !");
-                    return;
-                }
-
-                switch (reponse)
-                {
-                    case 1:
-                        
-                        break;
-                    case 2:
-                        Console.WriteLine("Retour au menu principal !");
-                        break;
-                    default:
-                        Console.WriteLine("Veuillez entrer 1, 2 ou 3.");
-                        break;
-                }
             });
         }
     }
