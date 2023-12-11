a90b517bf12b5d9a73b44f9b1de071841f6edba2
Théo Duflos <theo.duflos.yokoko@gmail.com>
2023-12-11 11:08:12 +0100

    fix :bug: Fixing route and display

--- banque/Affichage/AffichageOperationVirement.cs
+++ banque/Affichage/AffichageOperationVirement.cs
@@ -42,7 +42,7 @@ public class AffichageOperationVirement{
                                             numeroComptePourEnvoiVirement);
                     inputUtilisateurValide = false;
                     Thread.Sleep(2000);
-                    // Console.Clear();    
+                    Console.Clear();    
                     affichageCompte.Menu(client);
                 }
 
@@ -52,7 +52,7 @@ public class AffichageOperationVirement{
         catch (InvalidOperationException ex)
         {
             Thread.Sleep(2000);
-            // Console.Clear();
+            Console.Clear();
             
             affichageCompte.Menu(client);
             
